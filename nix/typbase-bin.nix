ead93f66cfa0a2db92466b33937afe88df088cd9
Lemuel DLS <noreply@git.lemueldls.dev>
2026-09-26 06:14:44 +0000

    chore: update nix flake to 0.2.4

--- nix/typbase-bin.nix
+++ nix/typbase-bin.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   src = fetchurl {
     url = "https://github.com/lemueldls/typbase/releases/download/typbase-v${finalAttrs.version}/Typbase_${finalAttrs.version}_amd64.deb";
-    sha256 = "sha256-yULio/ZWgUFHuq8J9NMGvphCe0+X+sISXXh/vUTJv6E=";
+    sha256 = "sha256-TLIrUgpPnkwzZ2Lta7YU7GoqrlWqhXOp1sUPxRAXCTA=";
   };
 
   dontConfigure = true;
